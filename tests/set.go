2e65820a9ee258167606b8cd3c25310577deb9f5
Kirill Yesikov <56737220+kirillyesikov@users.noreply.github.com>
2025-03-17 10:58:08 -0400

    Update set.go

--- tests/set.go
+++ tests/set.go
@@ -1,1 +1,45 @@
+package models
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
+)
+
+type PluginSettings struct {
+	Path    string                `json:"path"`
+	Secrets *SecretPluginSettings `json:"-"`
+}
+
+type SecretPluginSettings struct {
+	ApiKey string `json:"apiKey"`
+}
+
+func LoadPluginSettings(source backend.DataSourceInstanceSettings) (*PluginSettings, error) {
+	settings := PluginSettings{}
+	err := json.Unmarshal(source.JSONData, &settings)
+	if err != nil {
+		return nil, fmt.Errorf("could not unmarshal PluginSettings json: %w", err)
+	}
+
+	// Handling both values returned from loadSecretPluginSettings
+	settings.Secrets, err = loadSecretPluginSettings(source.DecryptedSecureJSONData)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load secret plugin settings: %w", err)
+	}
+
+	return &settings, nil
+}
+
+func loadSecretPluginSettings(source map[string]string) (*SecretPluginSettings, error) {
+	apiKey, exists := source["apiKey"]
+	if !exists || apiKey == "" {
+		return nil, fmt.Errorf("apiKey is missing or empty")
+	}
+
+	return &SecretPluginSettings{
+		ApiKey: apiKey,
+	}, nil
+}
 
