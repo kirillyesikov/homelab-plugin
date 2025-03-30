402c1e0c6c29803031db371915499e563c3cdfbb
Kirill Yesikov <56737220+kirillyesikov@users.noreply.github.com>
2025-03-30 17:49:55 -0400

    Update configEditor.spec.ts

--- tests/configEditor.spec.ts
+++ tests/configEditor.spec.ts
@@ -14,7 +14,7 @@ test('"Save & test" should be successful when configuration is valid', async ({
   const ds = await readProvisionedDataSource<MyDataSourceOptions, MySecureJsonData>({ fileName: 'datasources.yml' });
   const configPage = await createDataSourceConfigPage({ type: ds.type });
   await page.getByRole('textbox', { name: 'Path' }).fill(ds.jsonData.path ?? '');
-  await page.getByRole('textbox', { name: 'API Key' }).fill(ds.secureJsonData.apiKey ?? '');
+  await page.getByRole('textbox', { name: 'API Key' }).fill(ds.secureJsonData?.apiKey ?? '');
   await expect(configPage.saveAndTest()).toBeOK();
 });
 
