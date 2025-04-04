90e4e0fa458fc9aebae114061947feef7553bc63
Kirill Yesikov <56737220+kirillyesikov@users.noreply.github.com>
2025-04-04 18:08:00 -0400

    Update QueryEditor.tsx

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -7,20 +7,34 @@ import { MyDataSourceOptions, MyQuery } from '../types';
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
 export function QueryEditor({ query, onChange, onRunQuery }: Props) {
+  const onMetricChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange({ ...query, metric: event.target.value });
+    onRunQuery(); // execute query on metric change
+  };
+
   const onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
     onChange({ ...query, queryText: event.target.value });
   };
 
   const onConstantChange = (event: ChangeEvent<HTMLInputElement>) => {
     onChange({ ...query, constant: parseFloat(event.target.value) });
-    // executes the query
     onRunQuery();
   };
 
-  const { queryText, constant } = query;
+  const { metric, queryText, constant } = query;
 
   return (
     <Stack gap={0}>
+      <InlineField label="Metric" tooltip="Enter a Prometheus metric like go_threads or go_gc_duration_seconds">
+        <Input
+          id="query-editor-metric"
+          onChange={onMetricChange}
+          value={metric || ''}
+          placeholder="e.g. go_threads"
+          width={40}
+        />
+      </InlineField>
+
       <InlineField label="Constant">
         <Input
           id="query-editor-constant"
@@ -31,6 +45,7 @@ export function QueryEditor({ query, onChange, onRunQuery }: Props) {
           step="0.1"
         />
       </InlineField>
+
       <InlineField label="Query Text" labelWidth={16} tooltip="Not used yet">
         <Input
           id="query-editor-query-text"
@@ -43,3 +58,4 @@ export function QueryEditor({ query, onChange, onRunQuery }: Props) {
     </Stack>
   );
 }
+
